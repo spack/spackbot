5c60bc7fb7a5a188336be39e8b4f7d7e61817449
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2021-07-14 17:45:10 -0600

    Adding spackbot run pipeline to commands
    I forgot to add the record for the new commands in `@spackbot help` / `@spackbot commands`, so here is a quick PR to add it!

--- spackbot/pr_add_comments.py
+++ spackbot/pr_add_comments.py
@@ -39,6 +39,7 @@ You can interact with me in many ways!
 
 - `@spackbot hello`: say hello and get a friendly response back!
 - `@spackbot help` or `@spackbot commands`: see this message 
+- `@spackbot run pipeline` or `@spackbot re-run pipeline`: to request a new run of the GitLab CI pipeline 
 
 I'll also help to label your pull request and assign reviewers!
 If you need help or see there might be an issue with me, open an issue [here](https://github.com/spack/spack-bot/issues)
