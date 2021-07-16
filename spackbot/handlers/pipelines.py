a61dd68a6d2a41d33ca17d8b53279488f4f8cf57
vsoch <vsoch@users.noreply.github.com>
2021-07-16 10:27:40 -0600

    allowing the author of the PR to request a pipeline rerun
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- spackbot/handlers/pipelines.py
+++ spackbot/handlers/pipelines.py
@@ -35,9 +35,14 @@ async def run_pipeline(event, gh):
     sender = event.data["sender"]["login"]
     repository = event.data["repository"]
     collaborators_url = repository["collaborators_url"]
+    author = pr["user"]["login"]
+
+    # If it's the PR author, we allow it
+    if author == sender:
+        logger.info(f"Author {author} is requesting a pipeline run.")
 
     # If they don't have write, we don't allow the command
-    if not await found(gh.getitem(collaborators_url, {"collaborator": sender})):
+    elif not await found(gh.getitem(collaborators_url, {"collaborator": sender})):
         logger.info(f"Not found: {sender}")
         return (
             "Sorry %s, I cannot do that for you. Only users with write can make this request!"
