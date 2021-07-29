af8ea532158dd565e4e7943708b689b542b55f4e
vsoch <vsoch@users.noreply.github.com>
2021-07-28 18:10:32 -0600

    updates
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- spackbot/handlers/__init__.py
+++ spackbot/handlers/__init__.py
@@ -1,6 +1,6 @@
 from .pipelines import run_pipeline  # noqa
 from .labels import add_labels  # noqa
-from .reviewers import add_reviewers, add_issue_maintainers # noqa
+from .reviewers import add_reviewers, add_issue_maintainers  # noqa
 from .reviewers import add_reviewers  # noqa
 from .style import style_comment, fix_style  # noqa
-from .packages import count_packages # noqa
+from .packages import count_packages  # noqa
